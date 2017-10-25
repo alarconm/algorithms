428197400e6a71af94872da6f09388993b04b89d
Michael Alarcon <malarconii@yahoo.com>
2017-10-24 21:35:03 -0700

    finished recursive binary search all tests passing.

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,20 @@
 
+def binary_search(el, list, start=0, end=None):
+
+    if end is None:
+        end = len(list) - 1
+    if start > end:
+        return False
+
+    midpoint = (start + end)//2
+
+    if list[midpoint] == el:
+        return midpoint
+
+    elif el < list[midpoint]:
+        end = midpoint - 1
+        return binary_search(el, list, start, end)
+
+    elif el > list[midpoint]:
+        start = midpoint + 1
+        return binary_search(el, list, start, end)
--- test_binary_search.py
+++ test_binary_search.py
@@ -6,10 +6,7 @@ class TestBinarySearch(object):
         assert binary_search(3, [1,2,3,4,5,6,7,8],0,None) == 2
 
     def test_number_not_in_list(self):
-        assert binary_search(3, [1,2,4,5,6,7,8,9],0,None) == -1
-
-    def test_not_a_number(self):
-        assert binary_search('e', [1,2,3,4,5,6,7,8,9,10],0,None) == 'NaN'
+        assert binary_search(3, [1,2,4,5,6,7,8,9],0,None) == False
 
     def test_list_size_of_two(self):
         assert binary_search(3, [1,3],0,None) == 1
@@ -18,7 +15,7 @@ class TestBinarySearch(object):
         assert binary_search(-3,[-5,-4,-3,-2,-1,0,1],0,None) == 2
 
     def test_unsorted_list(self):
-        assert binary_search(3, [1,5,7,9,11,16,2,3],0,None) == 2
+        assert binary_search(3, [1,5,7,9,11,16,2,3],0,None) == False
 
     
 
